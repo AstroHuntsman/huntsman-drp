e906746b90c5bec6d962b1a763ed3256fc8559c7
Lee <lee.spitler@mq.edu.au>
2020-08-13 16:21:52 +1000

    pep8, autodoc, renamed to be more generic

--- src/huntsman/drp/calibs.py
+++ src/huntsman/drp/calibs.py
@@ -1,9 +1,19 @@
-
+"""Functions to interact with and create calibration data.
+"""
 from astropy import stats
 import astropy.io.fits as fits
 
 
-def get_calib_data_qualities(filename_list):
+def get_simple_image_data_stats(filename_list):
+    """Return a dictionary of simple stats for all
+    fits filenames in the input list.
+
+    Args:
+        filename_list (list): List of fits filenames.
+
+    Returns:
+        dict: mean, median, stdev for each fits file
+    """
     output_data_quality_dict = {}
     for filename in filename_list:
         mean, median, stdev = stats.sigma_clipped_stats(fits.getdata(filename))
