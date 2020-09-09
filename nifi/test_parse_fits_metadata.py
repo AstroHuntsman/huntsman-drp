afdb0d8bc98d8aa352024825cf376d6920d21719
Lee Spitler <lee.spitler@mq.edu.au>
2020-09-09 20:42:43 +1000

    add error

--- nifi/test_parse_fits_metadata.py
+++ nifi/test_parse_fits_metadata.py
@@ -2,13 +2,14 @@ import glob
 
 import parse_fits_metadata
 
+
 def main():
     basedir = "/var/huntsman/images/fields/*/*/*/*.fits*"
     for filename in glob.glob(basedir):
         try:
             parse_fits_metadata.main(filename, print_stdout=False)
-        except KeyError:
-            print(filename)
+        except KeyError as err:
+            print(filename, err)
 
 
 if __name__ == '__main__':
