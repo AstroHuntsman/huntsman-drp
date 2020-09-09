30a04edcd37f14f1b4a2831cfe3541ba1f25891a
Lee Spitler <lee.spitler@mq.edu.au>
2020-09-09 20:41:29 +1000

    fix import

--- nifi/test_parse_fits_metadata.py
+++ nifi/test_parse_fits_metadata.py
@@ -1,12 +1,6 @@
 import glob
 
-
-import importlib.util
-spec = importlib.util.spec_from_file_location("parse_fits_metadata",
-                                              "/home/huntsman/dan_scripts/test_nifi_script/huntsman-drp/nifi/parse_fits_metadata.py")
-parse_fits_metadata = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(parse_fits_metadata)
-
+import parse_fits_metadata
 
 def main():
     basedir = "/var/huntsman/images/fields/*/*/*/*.fits*"
