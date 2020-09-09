b2f55ca2f0f73a69f2a9ae3a73c50ad0648140d5
Lee Spitler <lee.spitler@mq.edu.au>
2020-09-09 20:46:47 +1000

    add other paths

--- nifi/test_parse_fits_metadata.py
+++ nifi/test_parse_fits_metadata.py
@@ -4,12 +4,16 @@ import parse_fits_metadata
 
 
 def main():
-    basedir = "/var/huntsman/images/fields/*/*/*/*.fits*"
-    for filename in glob.glob(basedir):
-        try:
-            parse_fits_metadata.main(filename, print_stdout=False)
-        except KeyError as err:
-            print(filename, err)
+    glob_strings = ["/var/huntsman/images/fields/*/*/*/*.fits*",
+                    "/var/huntsman/images/flats/*/*/*.fits*",
+                    "/var/huntsman/images/darks/*/*/*.fits*"]
+
+    for glob_string in glob_strings:
+        for filename in glob.glob(glob_string):
+            try:
+                parse_fits_metadata.main(filename, print_stdout=False)
+            except KeyError as err:
+                print(filename, err)
 
 
 if __name__ == '__main__':
