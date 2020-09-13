b2e39ba9aaad32586e3f0f3ee891068ec6ab2db2
fergus <fergus.longbottom@hdr.mq.edu.au>
2020-09-14 03:55:29 +1000

    dwf script

--- scripts/DWF_reduction.py
+++ scripts/DWF_reduction.py
@@ -14,36 +14,132 @@ for doing coadds.... also need skymapper catalogue for region of interest
 -will have a mounted output directory in the docker container, make this a
 parameter output_directory
 """
-
-
 from dateutil.parser import parse as parse_date
 import datetime.datetime as datetime
 from huntsman.drp.datatable import RawDataTable
 import argparse
 
 
-def main(date=None, exposure_time=300):
-    """Main function for DWF reduction script, which monitors for incoming
-    concurrent exposures and calibrates/coadds them.
+def ingest_master_bias(date, bias_config_file, datadir='DATA',
+                       calibdir='DATA/CALIB', rerun='processCcdOutputs',
+                       validity=1000):
+    """Ingest the master bias of a given date.""""
+
+    print(f"Ingesting master bias frames.")
+    cmd = f"ingestCalibs.py {datadir}"
+    cmd += f" {datadir}/rerun/{rerun}/calib/bias/{date}/*/*.fits"
+    cmd += f" --validity {validity}"
+    cmd += f" --calib {calibdir} --mode=link"
+    cmd += " --config clobber=True"
+    cmd += f" --configfile {bias_config_file}"
+    print(f'The ingest command is: {cmd}')
+    subprocess.call(cmd, shell=True)
+
+
+def ingest_master_flat(date, filter, flat_config_file, datadir='DATA',
+                       calibdir='DATA/CALIB', rerun='processCcdOutputs',
+                       validity=1000):
+    """Ingest the master flat of a given date."""
+    print(f"Ingesting master {filter} filter flats frames for ccd {ccd}.")
+    cmd = f"ingestCalibs.py {datadir}"
+    cmd += f" {datadir}/rerun/{rerun}/calib/flat/{date}/*/*.fits"
+    cmd += f" --validity {validity}"
+    cmd += f" --calib {calibdir} --mode=link"
+    cmd += " --config clobber=True"
+    cmd += f" --configfile {flat_config_file}"
+    print(f'The ingest command is: {cmd}')
+    subprocess.call(cmd, shell=True)
+
+
+def ingest_sci_images(file_list, datadir='DATA', calibdir='DATA/CALIB'):
+    """Ingest science images to be processed."""
+    cmd = f"ingestImages.py {datadir}"
+    cmd += f" testdata/science/*.fits --mode=link --calib {calibdir}"
+    print(f'The command is: {cmd}')
+    subprocess.call(cmd, shell=True)
+
+
+def processCcd(dataType='science', datadir='DATA', calibdir='DATA/CALIB',
+               rerun='processCcdOutputs'):
+    """Process ingested exposures."""
+    cmd = f"processCcd.py {datadir} --rerun {rerun}"
+    cmd += f" --calib {calibdir} --id dataType={dataType}"
+    print(f'The command is: {cmd}')
+    subprocess.call(cmd, shell=True)
+
+
+def makeDiscreteSkyMap(datadir='DATA', rerun='processCcdOutputs:coadd'):
+    """Create a sky map that covers processed exposures."""
+    cmd = f"makeDiscreteSkyMap.py {datadir}} --id --rerun {rerun} "
+    cmd += f"--config skyMap.projection="TAN""
+    subprocess.call(cmd, shell=True)
+
+
+def makeCoaddTempExp(filter, datadir='DATA', calibdir='DATA/CALIB',
+                     rerun='coadd'):
+    """Warp exposures onto sky map."""
+    cmd = f"makeCoaddTempExp.py {datadir} --rerun {rerun} "
+    cmd += f"--selectId filter={filter} --id filter={filter} tract=0 "
+    cmd += f"patch=0,0^0,1^0,2^1,0^1,1^1,2^2,0^2,1^2,2 "
+    cmd += f"--config doApplyUberCal=False"
+    print(f'The command is: {cmd}')
+    subprocess.call(cmd, shell=True)
+
+
+def assembleCoadd(filter, datadir='DATA', calibdir='DATA/CALIB',
+                  rerun='coadd'):
+    """Assemble the warped exposures into a coadd"""
+    cmd = f"assembleCoadd.py {datadir} --rerun {rerun} "
+    cmd += f"--selectId filter={filter} --id filter={filter} tract=0 "
+    cmd += f"patch=0,0^0,1^0,2^1,0^1,1^1,2^2,0^2,1^2,2"
+    print(f'The command is: {cmd}')
+    subprocess.call(cmd, shell=True)
+
+
+def process_concurrent_exposures(files, datadir='DATA', calibdir='DATA/CALIB',
+                                 validity=1000):
+    """Function that takes list of science exposures and processes them to
+    produce a coadd. Master calibs are assumed to have already been produced
+    amd ingested into butler repo. Skymapper catalogue is also assumed to have
+    been ingested.
 
     Args:
-        date (str, optional): Date to search for files.
-        date_range (int, optional): Number of days to search before date arg.
+        files (list): List of filepaths for processing.
+
+    TODO:
+        -Find way to handle exposures with different filters
     """
-    #add default option/exception handling in case config isnt set
-    rdb = RawDataTable()
+    flat_config_file = os.path.join(getPackageDir("obs_huntsman"), "config",
+                                    "ingestFlats.py")
 
-    if date is not None:
-        date = datetime.today()
-    date_parsed = parse_date(date)
-    date_range = datetime.timedelta(seconds=exposure_time)
+    bias_config_file = os.path.join(getPackageDir("obs_huntsman"), "config",
+                                    "ingestBiases.py")
 
+    ingest_master_bias(date, bias_config_file, datadir='DATA',
+                       calibdir='DATA/CALIB', rerun='processCcdOutputs',
+                       validity=1000)
 
+    ingest_master_flat(date, filter, flat_config_file, datadir='DATA',
+                       calibdir='DATA/CALIB', rerun='processCcdOutputs',
+                       validity=1000)
+
+    ingest_sci_images(file_list, datadir='DATA', calibdir='DATA/CALIB')
+
+    processCcd(dataType='science', datadir='DATA', calibdir='DATA/CALIB',
+               rerun='processCcdOutputs')
+
+    makeDiscreteSkyMap(datadir='DATA', rerun='processCcdOutputs:coadd')
+
+    makeCoaddTempExp(filter, datadir='DATA', calibdir='DATA/CALIB',
+                     rerun='coadd')
+
+    assembleCoadd(filter, datadir='DATA', calibdir='DATA/CALIB',
+                  rerun='coadd')
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('raw-db', type=str, help='raw database file location.')
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('abc', type=str, help='abc.')
+    # args = parser.parse_args()
 
-    main()
+    process_concurrent_exposures(files)
