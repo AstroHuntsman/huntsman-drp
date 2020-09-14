2db797d7d6e400876ecf2fe819b4cd1166cdbf2d
Dan <danjampro@sky.com>
2020-09-14 14:53:16 +1000

    use function

--- scripts/dwf/process_new_data.py
+++ scripts/dwf/process_new_data.py
@@ -5,13 +5,17 @@ from threading import Thread
 from datetime import datetime, timedelta
 
 from huntsman.drp.datatable import RawDataTable
+from reduction import process_concurrent_exposures
 
 
 def query_latest_files(datatable, interval):
-    """ Get latest filenames specified by a time interval.
+    """
+    Get latest filenames specified by a time interval.
+
     Args:
         datatable (`huntsman.drp.datatable.RawDataTable`): The raw data table.
         interval (float): The time interval in seconds.
+
     Returns:
         list of filenames.
     """
@@ -21,10 +25,12 @@ def query_latest_files(datatable, interval):
     return filenames
 
 
-def process_data_async(queue):
+def process_data(queue):
+    """Get queued filename list and start processing it."""
     while True:
+        date = datetime.utcnow().strftime('%Y-%m-%d')
         filenames = queue.get()
-        process_data(filenames)
+        process_concurrent_exposures(filenames, master_bias_date=date, master_flat_date=date)
         queue.task_done()
 
 
@@ -37,7 +43,7 @@ if __name__ == "__main__":
     queue = Queue()
 
     # Start the queue's worker thread
-    thread = Thread(target=process_data_async, daemon=False, args=(queue))
+    thread = Thread(target=process_data, daemon=False, args=(queue))
 
     while True:
 
