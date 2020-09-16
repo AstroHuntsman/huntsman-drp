4ec8687fa7552db979a02c44b0b380df7a02d33f
Dan <danjampro@sky.com>
2020-09-16 16:00:28 +1000

    stuff

--- scripts/dwf/make_master_calibs.py
+++ scripts/dwf/make_master_calibs.py
@@ -3,11 +3,10 @@ import argparse
 from datetime import datetime, timedelta
 
 from huntsman.drp.datatable import RawDataTable
-# from huntsman.drp.butler import TemporaryButlerRepository
 from huntsman.drp.butler import ButlerRepository
 
 
-def get_recent_calibs(interval):
+def get_recent_calibs(interval, **kwargs):
     """Get the most recent calibration images."""
     datatable = RawDataTable()
     date_end = datetime.utcnow()
@@ -15,16 +14,20 @@ def get_recent_calibs(interval):
 
     # Get bias filenames
     filenames_bias = datatable.query_column("filename", date_start=date_start, date_end=date_end,
-                                            dataType="bias")
+                                            dataType="bias", **kwargs)
     print(f"Found {len(filenames_bias)} bias fields.")
 
     # Get flat filenames
+    """
     # This is a hack to cope with the non-standard field naming
     metalist = datatable.query(date_start=date_start, date_end=date_end)
     filenames_flat = []
     for m in metalist:
         if m["FIELD"].startswith("Flat") and not m["dataType"] == "bias":
             filenames_flat.append(m["filename"])
+    """
+    filenames_flat = datatable.query_column("filename", date_start=date_start, date_end=date_end,
+                                            dataType="flat", **kwargs)
     print(f"Found {len(filenames_flat)} flat fields.")
 
     return [*filenames_bias, *filenames_flat]
@@ -42,10 +45,11 @@ if __name__ == "__main__":
     # Get filenames
     filenames = get_recent_calibs(interval)
 
+    # Make butler repository
     butler_repo = ButlerRepository("/opt/lsst/software/stack/DATA")
 
     # Ingest raw data
-    butler_repo.ingest_raw_data(filenames, ignore_ingested=True)
+    butler_repo.ingest_raw_data(filenames, ignore_ingested=True, ccd=2)
 
     # Make master calibs
     butler_repo.make_master_calibs(calib_date=datetime.utcnow(), rerun=rerun)
