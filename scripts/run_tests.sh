545bde2c806c14f2f7a3cd03ad80af29c9c2d8a9
Dan <danjampro@sky.com>
2020-10-15 14:23:54 +1100

    something

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -10,6 +10,6 @@ SESSION_FILE=~/pytest_session.txt
 cd ${HUNTSMAN_DRP}/src/huntsman/drp
 
 pytest -x --cov=huntsman.drp --cov-report html:${COVERAGE_REPORT_HTML} \
-  --cov-report xml:${COVERAGE_REPORT_XML} --cov-config=.coveragerc --session2file=${SESSION_FILE}
+  --cov-config=.coveragerc --session2file=${SESSION_FILE}
 
 exit 0
