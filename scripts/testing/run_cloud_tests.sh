0e5a35c08728fbf6607786c403e4f34da60b9c3a
Dan <danjampro@sky.com>
2020-10-16 10:07:11 +1100

    specify compose file

--- scripts/testing/run_cloud_tests.sh
+++ scripts/testing/run_cloud_tests.sh
@@ -3,7 +3,7 @@ set -eu
 
 LSST_HOME=/opt/lsst/software/stack
 
-docker-compose -f ${HUNTSMAN_DRP}/docker/testing/docker-compose.yaml run --rm \
+docker-compose -f ${HUNTSMAN_DRP}/docker/testing/docker-compose.yml run --rm \
   -e "HUNTSMAN_COVERAGE=${LSST_HOME}/coverage" \
   -v ${COVERAGE_DIR}:${LSST_HOME}/coverage \
   python_tests ${LSST_HOME}/huntsman-drp/scripts/testing/run_local_tests.sh
