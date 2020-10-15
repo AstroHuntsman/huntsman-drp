c34d895b91a3f47406515d098c2f0362b5b9804a
Dan <danjampro@sky.com>
2020-10-15 21:42:08 +1100

    coverage rott

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,18 +2,15 @@
 source ~/.bashrc
 set -eu
 
-COVERAGE_REPORT_HTML=${HUNTSMAN_DRP}/htmlcov
-COVERAGE_CONFIG=${HUNTSMAN_DRP}/src/huntsman/drp/.coveragerc
-COVERAGE_REPORT_XML=${HUNTSMAN_DRP}/coverage.xml
-SESSION_FILE=~/pytest_session.txt
+# Coverage root set by first command line arg. If not given, use package root.
+COVERAGE_ROOT=${1:-${HUNTSMAN_DRP}}
+cd ${COVERAGE_ROOT}
 
-# cd ${HUNTSMAN_DRP}/src/huntsman/drp
-cd ${LSST_HOME}
-
-pytest ${HUNTSMAN_DRP} -x --cov=huntsman.drp \
-          --cov-config=${COVERAGE_CONFIG} \
-          --cov-report xml:${COVERAGE_REPORT_XML} \
-          --cov-report html:${COVERAGE_REPORT_HTML} \
-          --session2file=${SESSION_FILE}
+pytest ${HUNTSMAN_DRP} -x \
+  --cov=huntsman.drp \
+  --cov-config=${HUNTSMAN_DRP}/src/huntsman/drp/.coveragerc \
+  --cov-report xml:${COVERAGE_ROOT}/coverage.xml \
+  --cov-report html:${COVERAGE_ROOT}/htmlcov \
+  --session2file=${COVERAGE_ROOT}/pytest_session.txt
 
 exit 0
