43a44d908c3557e3ba8055445bbd6fa795cb171e
Dan <danjampro@sky.com>
2020-10-15 11:57:45 +1100

    session file

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -5,10 +5,11 @@ set -eu
 
 COVERAGE_REPORT_HTML=${HUNTSMAN_DRP}/htmlcov
 COVERAGE_REPORT_XML=${HUNTSMAN_DRP}/coverage.xml
+SESSION_FILE=${HUNTSMAN_DRP}/pytest_session.txt
 
 cd ${HUNTSMAN_DRP}/src/huntsman/drp
 
 pytest -x --cov=huntsman.drp --cov-report html:${COVERAGE_REPORT_HTML} \
-  --cov-report xml:${COVERAGE_REPORT_XML} --cov-config=.coveragerc
+  --cov-report xml:${COVERAGE_REPORT_XML} --cov-config=.coveragerc --session2file=${SESSION_FILE}
 
 exit 0
