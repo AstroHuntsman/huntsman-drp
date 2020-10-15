d827300a732ef2f10f4f6ec6a4b9cf340e15f76f
Dan <danjampro@sky.com>
2020-10-15 19:47:21 +1100

    downgrade coverage

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,14 +4,17 @@ source ~/.bashrc
 set -eu
 
 COVERAGE_REPORT_HTML=${HUNTSMAN_DRP}/htmlcov
+COVERAGE_CONFIG=${HUNTSMAN_DRP}/src/huntsman/drp/.coveragerc
 COVERAGE_REPORT_XML=${HUNTSMAN_DRP}/coverage.xml
 SESSION_FILE=~/pytest_session.txt
 
+
 cd ${HUNTSMAN_DRP}/src/huntsman/drp
 
-pytest -x
-
-#pytest -x --cov=huntsman.drp --cov-report html:${COVERAGE_REPORT_HTML} \
-#  --cov-config=.coveragerc --session2file=${SESSION_FILE}
+pytest -x --cov=huntsman.drp \
+          --cov-config=${COVERAGE_CONFIG} \
+          --cov-report xml:${COVERAGE_REPORT_XML} \
+          --cov-report html:${COVERAGE_REPORT_HTML} \
+          --session2file=${SESSION_FILE}
 
 exit 0
