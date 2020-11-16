b4eacd5a32695a4036c95ddc29acec1718624561
Dan <danjampro@sky.com>
2020-11-16 16:36:09 +1100

    remove unused functions

--- src/huntsman/drp/datatable.py
+++ src/huntsman/drp/datatable.py
@@ -72,7 +72,7 @@ class DataTable(HuntsmanBase):
         self._db = self._client[db_name]
         self._table = self._db[table_name]
 
-    def find(self, criteria, expected_count=None):
+    def query(self, criteria, expected_count=None):
         """
         Find metadata for one or more matches in a table.
         Args:
@@ -91,38 +91,6 @@ class DataTable(HuntsmanBase):
         if expected_count is not None:
             if df.shape[0] != expected_count:
                 raise RuntimeError(f"Expected {expected_count} matches but found {df.shape[0]}.")
-        return df
-
-    def query(self, date=None, date_start=None, date_end=None, criteria=None):
-        """
-        Query the table, optionally with a date range.
-        Args:
-            date (date, optional): The specific date to query on.
-            date_start (date, optional): The earliest date of returned rows.
-            date_end (date, optional): The latest date of returned rows.
-            query_dict (abc.Mapping, optional): Parsed to the query.
-        Returns:
-            list of dict: Dictionary of query results.
-        """
-        df = self.find(criteria=criteria)
-
-        # Apply date selection using parse_date
-        # TODO remove this in favour of pymongo date handling
-        if self._date_key in df.columns:
-
-            # Build query criteria
-            date_criteria = {}
-            if date is not None:
-                date_criteria["equals"] = parse_date(date)
-            if date_start is not None:
-                date_criteria["greater_than_equals"] = parse_date(date_start)
-            if date_end is not None:
-                date_criteria["less_than"] = parse_date(date_end)
-
-            # Apply to dataframe
-            parsed_dates = np.array([parse_date(d) for d in df[self._date_key].values])
-            keep = Criteria(date_criteria).is_satisfied(parsed_dates)
-            df = df[keep].reset_index(drop=True)
 
         self.logger.debug(f"Query returned {df.shape[0]} results.")
         return df
@@ -141,31 +109,6 @@ class DataTable(HuntsmanBase):
         date_now = current_date()
         date_start = date_now - timedelta(days=days, hours=hours, seconds=seconds)
         return self.query(date_start=date_start, criteria=criteria)
-
-    def query_matches(self, values, match_key="filename", one_to_one=True, **kwargs):
-        """ Get rows matching values of a certain key.
-        Args:
-            table (huntsman.drp.datatable.DataTable): The data table to match with.
-            match_key (str, optional): The key to match on. Default: 'filename'.
-            one_to_one (bool): If True (default), require one-to-one matching.
-            **kwargs: Parsed to table.query
-        Returns:
-            pd.DataFrame: The matched query result.
-        """
-        df_query = self.query(**kwargs)
-
-        # Use the matching key as the DataFrame index
-        df_query.set_index(match_key, inplace=True)
-
-        # Return the matched DataFrame.
-        df_matched = pd.DataFrame([df_query.loc[v] for v in values])
-        df_matched[match_key] = values
-
-        if one_to_one:
-            if df_matched.shape[0] != len(values):
-                raise RuntimeError("One-to-one criteria not satisfied for matching query.")
-
-        return df_matched
 
     @edit_permission_validation
     @new_document_validation
@@ -232,32 +175,6 @@ class DataTable(HuntsmanBase):
             raise RuntimeError(f"Unexpected number of documents deleted: {result.deleted_count}.")
 
         return result
-
-    def update_file_data(self, filename, data, **kwargs):
-        """
-        Update the metadata associated with a file in the database.
-        Args:
-            filename (str): Modify the metadata for this file.
-            data (dict): Dictionary of key: value pairs to update in the database. The field will
-                be created if it does not already exist.
-        Returns:
-            `pymongo.results.UpdateResult`: The result of the update operation.
-        """
-        data_id = {'filename': filename}
-        return self.update_document(data_id, data, **kwargs)
-
-    def delete_file_data(self, filename, **kwargs):
-        """
-        Delete the metadata associated with a file in the database.
-        Args:
-            filename (str): Modify the metadata for this file.
-            data (dict): Dictionary of key: value pairs to update in the database. The field will
-                be created if it does not already exist.
-        Returns:
-            `pymongo.results.UpdateResult`: The result of the delete operation.
-        """
-        data_id = {'filename': filename}
-        return self.delete_document(data_id, **kwargs)
 
     def _validate_edit_permission(self, bypass_allow_edits=False, **kwargs):
         """Raise a PermissionError if not `bypass_allow_edits` or `self._allow_edits`."""
