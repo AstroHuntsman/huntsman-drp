0e269ae4b258ed2663a0d52924c6445b83aeacd3
Dan <danjampro@sky.com>
2020-11-20 16:40:19 +1100

    ingest task

--- src/huntsman/drp/lsst/ingest_refcat_task.py
+++ src/huntsman/drp/lsst/ingest_refcat_task.py
@@ -1,7 +1,6 @@
 """ Minimal overrides to remove unnecessary and time consuming file lock creation in current
 implementation of ingestIndexReferenceTask."""
 
-from ctypes import c_int
 import multiprocessing
 
 import astropy.units as u
@@ -9,12 +8,6 @@ import numpy as np
 
 from lsst.meas.algorithms import IngestIndexedReferenceTask
 from lsst.meas.algorithms.ingestIndexManager import IngestIndexManager
-
-# global shared counter to keep track of source ids
-# (multiprocess sharing is most easily done with a global)
-COUNTER = multiprocessing.Value(c_int, 0)
-# global shared counter to keep track of number of files processed.
-FILE_PROGRESS = multiprocessing.Value(c_int, 0)
 
 
 class singleProccessIngestIndexManager(IngestIndexManager):
@@ -58,6 +51,7 @@ class singleProccessIngestIndexManager(IngestIndexManager):
         if self.config.coord_err_unit is not None:
             # cache this to speed up coordinate conversions
             self.coord_err_unit = u.Unit(self.config.coord_err_unit)
+        self._couter = 0
 
     def run(self, inputFiles):
         """Index a set of input files from a reference catalog, and write the
@@ -67,12 +61,13 @@ class singleProccessIngestIndexManager(IngestIndexManager):
         inputFiles : `list`
             A list of file paths to read data from.
         """
-        global COUNTER, FILE_PROGRESS
         self.nInputFiles = len(inputFiles)
-        COUNTER.value = 0
-        FILE_PROGRESS.value = 0
-        for filename in inputFiles:
-            self._ingestOneFile(filename)
+
+        with multiprocessing.Manager():
+            self._counter = 0
+            self._file_progress = 0
+            for filename in inputFiles:
+                self._ingestOneFile(filename)
 
     def _ingestOneFile(self, filename):
         """Read and process one file, and write its records to the correct
@@ -82,7 +77,6 @@ class singleProccessIngestIndexManager(IngestIndexManager):
         filename : `str`
             The file to process.
         """
-        global FILE_PROGRESS
         inputData = self.file_reader.run(filename)
         fluxes = self._getFluxes(inputData)
         coordErr = self._getCoordErr(inputData)
@@ -91,16 +85,71 @@ class singleProccessIngestIndexManager(IngestIndexManager):
         pixel_ids = set(matchedPixels)
         for pixelId in pixel_ids:
             self._doOnePixel(inputData, matchedPixels, pixelId, fluxes, coordErr)
-        with FILE_PROGRESS.get_lock():
-            oldPercent = 100 * FILE_PROGRESS.value / self.nInputFiles
-            FILE_PROGRESS.value += 1
-            percent = 100 * FILE_PROGRESS.value / self.nInputFiles
-            # only log each "new percent"
-            if np.floor(percent) - np.floor(oldPercent) >= 1:
-                self.log.info("Completed %d / %d files: %d %% complete ",
-                              FILE_PROGRESS.value,
-                              self.nInputFiles,
-                              percent)
+
+        oldPercent = 100 * self._file_progress / self.nInputFiles
+        self._file_progress += 1
+        percent = 100 * self._file_progress / self.nInputFiles
+        # only log each "new percent"
+        if np.floor(percent) - np.floor(oldPercent) >= 1:
+            self.log.info("Completed %d / %d files: %d %% complete ",
+                          self._file_progress,
+                          self.nInputFiles,
+                          percent)
+
+    def _doOnePixel(self, inputData, matchedPixels, pixelId, fluxes, coordErr):
+        """Process one HTM pixel, appending to an existing catalog or creating
+        a new catalog, as needed.
+        Parameters
+        ----------
+        inputData : `numpy.ndarray`
+            The data from one input file.
+        matchedPixels : `numpy.ndarray`
+            The row-matched pixel indexes corresponding to ``inputData``.
+        pixelId : `int`
+            The pixel index we are currently processing.
+        fluxes : `dict` [`str`, `numpy.ndarray`]
+            The values that will go into the flux and fluxErr fields in the
+            output catalog.
+        coordErr : `dict` [`str`, `numpy.ndarray`]
+            The values that will go into the coord_raErr, coord_decErr, and
+            coord_ra_dec_Cov fields in the output catalog (in radians).
+        """
+        idx = np.where(matchedPixels == pixelId)[0]
+        catalog = self.getCatalog(pixelId, self.schema, len(idx))
+        for outputRow, inputRow in zip(catalog[-len(idx):], inputData[idx]):
+            self._fillRecord(outputRow, inputRow)
+
+        self._setIds(inputData[idx], catalog)
+
+        # set fluxes from the pre-computed array
+        for name, array in fluxes.items():
+            catalog[self.key_map[name]][-len(idx):] = array[idx]
+
+        # set coordinate errors from the pre-computed array
+        for name, array in coordErr.items():
+            catalog[name][-len(idx):] = array[idx]
+
+        catalog.writeFits(self.filenames[pixelId])
+
+    def _setIds(self, inputData, catalog):
+        """Fill the `id` field of catalog with a running index, filling the
+        last values up to the length of ``inputData``.
+        Fill with `self.config.id_name` if specified, otherwise use the
+        global running counter value.
+        Parameters
+        ----------
+        inputData : `numpy.ndarray`
+            The input data that is being processed.
+        catalog : `lsst.afw.table.SimpleCatalog`
+            The output catalog to fill the ids.
+        """
+        size = len(inputData)
+        if self.config.id_name:
+            catalog['id'][-size:] = inputData[self.config.id_name]
+        else:
+            idEnd = self._counter + size
+            catalog['id'][-size:] = np.arange(self._counter, idEnd)
+            self._counter = idEnd
 
 
 class HuntsmanIngestIndexedReferenceTask(IngestIndexedReferenceTask):
