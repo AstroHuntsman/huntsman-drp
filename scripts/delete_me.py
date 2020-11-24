b77922665fc15e3745813144f15c6fa032b15473
Dan <danjampro@sky.com>
2020-11-24 13:22:34 +1100

    select ccd 8

--- scripts/delete_me.py
+++ scripts/delete_me.py
@@ -1,5 +1,8 @@
+import numpy as np
+
 from huntsman.drp.datatable import RawDataTable, MasterCalibTable
 from huntsman.drp.butler import ButlerRepository
+
 
 if __name__ == "__main__":
 
@@ -7,7 +10,11 @@ if __name__ == "__main__":
     mct = MasterCalibTable()
     br = ButlerRepository("/home/lsst/test-butler-repo")
 
-    fnames_sci = rdt.query({"dataType": "science", "expTime": 60, "ccd": 8})["filename"].values[:1]
+    df = rdt.query({"dataType": "science", "expTime": 60, "ccd": 8})
+    cond = np.isfinite(df["RA-MNT"].values)
+    df = df[cond].reset_index(drop=True)
+    fnames_sci = df["filename"].values[:1]
+
     fnames_bias = mct.query({"datasetType": "bias"})["filename"].values
     fnames_flat = mct.query({"datasetType": "flat"})["filename"].values
 
