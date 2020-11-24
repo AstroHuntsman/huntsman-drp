351b106b2935ea058f73b65de045184f4c386e00
Dan <danjampro@sky.com>
2020-11-25 10:34:43 +1100

    well fullfrac

--- src/huntsman/drp/quality.py
+++ src/huntsman/drp/quality.py
@@ -10,18 +10,20 @@ METRICS = "clipped_stats", "flipped_asymmetry"  # TODO: Refactor!
 QUALITY_FLAG_NAME = "quality_success_flag"
 
 
-def metadata_from_fits(filename, config=None, logger=None, dtype="float32"):
+def metadata_from_fits(file_info, config=None, logger=None, dtype="float32"):
     """ Return a dictionary of data quality metrics for the file. A flag is added to indicate if
     all metics were calculated successfully.
     Args:
-        filename (str): Filename of FITS image.
+        file_info (abc.Mapping): Filename of FITS image.
         dtype (str or Type): Convert the image data to this type before processing.
     Returns:
         dict: A dictionary of metadata key: value pairs, including the filename.
     """
     if logger is None:
         logger = get_logger()
-    logger.debug(f"Calculating metadata for {filename}.")
+    filename = file_info["filename"]
+
+    logger.debug(f"Calculating data quality metrics for {filename}.")
     result = {"filename": filename, QUALITY_FLAG_NAME: True}
 
     # Load the data from file
@@ -36,7 +38,7 @@ def metadata_from_fits(filename, config=None, logger=None, dtype="float32"):
     for metric_name in METRICS:
         logger.debug(f"Calcualating metric for {filename}: {metric_name}.")
         try:
-            result.update(globals()[metric_name](data, config=config))
+            result.update(globals()[metric_name](data, file_info, config=config, logger=logger))
         except Exception as err:
             logger.error(f"Problem getting '{metric_name}' metric for {filename}: {err}")
             result[QUALITY_FLAG_NAME] = False
@@ -44,10 +46,20 @@ def metadata_from_fits(filename, config=None, logger=None, dtype="float32"):
     return result
 
 
-def clipped_stats(data, **kwargs):
+def clipped_stats(data, file_info, logger, **kwargs):
     """ Return sigma-clipped image statistics. """
     mean, median, stdev = stats.sigma_clipped_stats(data)
-    return {"clipped_mean": mean, "clipped_median": median, "clipped_std": stdev}
+
+    # Calculate the well fullness fraction using clipped median
+    try:
+        saturate = float(file_info["SATURATE"])
+    except KeyError:
+        logger.warning("No SATURATE key found in file metadata. Using 4094.")
+        saturate = 4094.
+    well_fullfrac = median / saturate
+
+    return {"clipped_mean": mean, "clipped_median": median, "clipped_std": stdev,
+            "well_fullfrac": median}
 
 
 def flipped_asymmetry(data, **kwargs):
