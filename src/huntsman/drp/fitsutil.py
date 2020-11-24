7b7340f559fa20912e814280d2a4df422ad64b88
Dan <danjampro@sky.com>
2020-11-24 13:54:34 +1100

    fits

--- src/huntsman/drp/fitsutil.py
+++ src/huntsman/drp/fitsutil.py
@@ -6,19 +6,34 @@ from huntsman.drp.base import HuntsmanBase
 from huntsman.drp.utils.date import parse_date
 
 
-def read_fits_header(filename):
+def read_fits_header(filename, ext="auto"):
     """ Read the FITS header for a given filename.
     Args:
         filename (str): The filename.
+        ext (str or int): Which FITS extension to use. If 'auto' (default), will choose based on
+            file extension. If 'all', will recursively extend the header with all extensions.
+            Else, will use int(ext) as the ext number.
     Returns:
         dict: The header dictionary.
     """
-    if filename.endswith(".fits"):
-        ext = 0
-    elif filename.endswith(".fits.fz"):  # <----- CHECK THIS
-        ext = 1
+    if ext == "all":
+        header = fits.Header()
+        i = 0
+        while True:
+            try:
+                header.extend(fits.getheader(filename, ext=i))
+            except IndexError:
+                if i > 1:
+                    return header
+    elif ext == "auto":
+        if filename.endswith(".fits"):
+            ext = 0
+        elif filename.endswith(".fits.fz"):  # <----- CHECK THIS
+            ext = 1
+        else:
+            raise ValueError(f"Unrecognised FITS extension for {filename}.")
     else:
-        raise ValueError(f"Unrecognised FITS extension for {filename}.")
+        ext = int(ext)
     return fits.getheader(filename, ext=ext)
 
 
