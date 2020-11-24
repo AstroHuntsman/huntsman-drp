ca5b66d45a5e237840e4dd6176b32a05b9241897
Dan <danjampro@sky.com>
2020-11-24 12:57:28 +1100

    test script

---
+++ scripts/delete_me.py
@@ -0,0 +1,20 @@
+from huntsman.drp.datatable import RawDataTable, MasterCalibTable
+from huntsman.drp.butler import ButlerRepository
+
+if __name__ == "__main__":
+
+    rdt = RawDataTable()
+    mct = MasterCalibTable()
+    br = ButlerRepository("/home/lsst/test-butler-repo")
+
+    fnames_sci = rdt.query({"dataType": "science", "expTime": 60})["filename"].values[:1]
+    fnames_bias = mct.query({"datasetType": "bias"})["filename"].values
+    fnames_flat = mct.query({"datasetType": "flat"})["filename"].values
+
+    br.ingest_raw_data(fnames_sci)
+    br.ingest_master_calibs("bias", fnames_bias)
+    br.ingest_master_calibs("flat", fnames_flat)
+
+    br.make_reference_catalogue()
+
+    br.make_calexps()
