fbf6b260cc3644fb3d4a14c37170672294481c2c
Dan <danjampro@sky.com>
2021-03-11 10:41:39 +1100

    update docker files

--- scripts/testing/run-local-tests.sh
+++ scripts/testing/run-local-tests.sh
@@ -12,6 +12,10 @@ function cleanup {
 
 mkdir -p ${HUNTSMAN_DRP_COVDIR} && chmod -R 777 ${HUNTSMAN_DRP_COVDIR}
 
+echo "Building new docker image for testing..."
+docker-compose -f ${COMPOSE_FILE} build python-tests
+
+echo "Running python tests inside docker container..."
 trap cleanup EXIT
 docker-compose -f ${COMPOSE_FILE} run --rm \
   -e "HUNTSMAN_COVERAGE=/opt/lsst/software/stack/coverage" \
