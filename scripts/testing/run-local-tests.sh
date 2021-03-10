8428b260ed39c15c8fbabe83ad021532aaf5ada5
Dan <danjampro@sky.com>
2021-03-11 10:26:21 +1100

    update docker files

--- scripts/testing/run-local-tests.sh
+++ scripts/testing/run-local-tests.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# This script is run from outside of the docker environment and is useful for running
-# local tests.
+# This script is run from outside of the docker environment
 set -eu
 
 HUNTSMAN_DRP_COVDIR=${HUNTSMAN_DRP_COVDIR:-${HUNTSMAN_DRP}/coverage}
@@ -17,4 +16,4 @@ trap cleanup EXIT
 docker-compose -f ${COMPOSE_FILE} run --rm \
   -e "HUNTSMAN_COVERAGE=/opt/lsst/software/stack/coverage" \
   -v "${HUNTSMAN_DRP_COVDIR}:/opt/lsst/software/stack/coverage" \
-  python_tests
+  python-tests
