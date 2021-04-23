5b6fde111b0ffe43871522240fdf4ce26aa8747b
Dan <danjampro@sky.com>
2021-04-23 12:37:04 +1000

    multiproc calexp

--- src/huntsman/drp/tests/test_calexp.py
+++ src/huntsman/drp/tests/test_calexp.py
@@ -5,13 +5,14 @@ import numpy as np
 from huntsman.drp.services.calexp import CalexpQualityMonitor
 
 
-def test_calexp_quality_monitor(exposure_collection_real_data, master_calib_collection_real_data, config):
+def test_calexp_quality_monitor(exposure_collection_real_data, master_calib_collection_real_data,
+                                config):
     """ Test that the quality monitor is able to calculate and archive calexp metrics. """
     refcat_filename = os.path.join(config["directories"]["testdata"], "refcat.csv")
 
     n_to_process = exposure_collection_real_data.count_documents({"dataType": "science"})
     m = CalexpQualityMonitor(exposure_collection=exposure_collection_real_data,
-                             refcat_filename=refcat_filename, sleep=5,
+                             refcat_filename=refcat_filename, status_interval=5, queue_interval=5,
                              calib_collection=master_calib_collection_real_data)
     m.start()
     i = 0
