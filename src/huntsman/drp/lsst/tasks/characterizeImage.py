ec238e0ce9b610df9f6b17ab5464f24a46cfb4a6
Dan <danjampro@sky.com>
2021-05-04 15:59:38 +1000

    first

---
+++ src/huntsman/drp/lsst/tasks/characterizeImage.py
@@ -0,0 +1,252 @@
+""" Huntsman overrides for CharacterizeImageTask.
+
+Changes:
+- Adds try except statement around PSF measurement. This allows the source catalogue to be written
+  in case it fails.
+"""
+import numpy as np
+
+
+import lsst.pipe.base as pipeBase
+from lsst.obs.base import ExposureIdInfo
+from lsst.afw.math import BackgroundList
+from lsst.afw.table import SourceTable, IdFactory
+from lsst.pex.exceptions import LengthError
+from lsst.pipe.tasks.characterizeImage import CharacterizeImageTask
+
+
+class HuntsmanCharacterizeImageTask(CharacterizeImageTask):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @pipeBase.timeMethod
+    def runDataRef(self, dataRef, exposure=None, background=None, doUnpersist=True):
+        """!Characterize a science image and, if wanted, persist the results
+
+        This simply unpacks the exposure and passes it to the characterize method to do the work.
+
+        @param[in] dataRef: butler data reference for science exposure
+        @param[in,out] exposure  exposure to characterize (an lsst.afw.image.ExposureF or similar).
+            If None then unpersist from "postISRCCD".
+            The following changes are made, depending on the config:
+            - set psf to the measured PSF
+            - set apCorrMap to the measured aperture correction
+            - subtract background
+            - interpolate over cosmic rays
+            - update detection and cosmic ray mask planes
+        @param[in,out] background  initial model of background already subtracted from exposure
+            (an lsst.afw.math.BackgroundList). May be None if no background has been subtracted,
+            which is typical for image characterization.
+            A refined background model is output.
+        @param[in] doUnpersist  if True the exposure is read from the repository
+            and the exposure and background arguments must be None;
+            if False the exposure must be provided.
+            True is intended for running as a command-line task, False for running as a subtask
+
+        @return same data as the characterize method
+        """
+        self._frame = self._initialFrame  # reset debug display frame
+        self.log.info("Processing %s" % (dataRef.dataId))
+
+        if doUnpersist:
+            if exposure is not None or background is not None:
+                raise RuntimeError("doUnpersist true; exposure and background must be None")
+            exposure = dataRef.get("postISRCCD", immediate=True)
+        elif exposure is None:
+            raise RuntimeError("doUnpersist false; exposure must be provided")
+
+        exposureIdInfo = dataRef.get("expIdInfo")
+
+        charRes = self.run(exposure=exposure, exposureIdInfo=exposureIdInfo, background=background)
+
+        if self.config.doWrite:
+            dataRef.put(charRes.sourceCat, "icSrc")
+            if self.config.doWriteExposure:
+                dataRef.put(charRes.exposure, "icExp")
+                dataRef.put(charRes.background, "icExpBackground")
+
+        if not charRes.psfSuccess:
+            raise RuntimeError("Unable to measure PSF. See logs for details.")
+
+        return charRes
+
+    @pipeBase.timeMethod
+    def run(self, exposure, exposureIdInfo=None, background=None):
+        """!Characterize a science image
+
+        Peforms the following operations:
+        - Iterate the following config.psfIterations times, or once if config.doMeasurePsf false:
+            - detect and measure sources and estimate PSF (see detectMeasureAndEstimatePsf for
+              details)
+        - interpolate over cosmic rays
+        - perform final measurement
+
+        @param[in,out] exposure  exposure to characterize (an lsst.afw.image.ExposureF or similar).
+            The following changes are made:
+            - update or set psf
+            - set apCorrMap
+            - update detection and cosmic ray mask planes
+            - subtract background and interpolate over cosmic rays
+        @param[in] exposureIdInfo  ID info for exposure (an lsst.obs.base.ExposureIdInfo).
+            If not provided, returned SourceCatalog IDs will not be globally unique.
+        @param[in,out] background  initial model of background already subtracted from exposure
+            (an lsst.afw.math.BackgroundList). May be None if no background has been subtracted,
+            which is typical for image characterization.
+
+        @return pipe_base Struct containing these fields, all from the final iteration
+        of detectMeasureAndEstimatePsf:
+        - exposure: characterized exposure; image is repaired by interpolating over cosmic rays,
+            mask is updated accordingly, and the PSF model is set
+        - sourceCat: detected sources (an lsst.afw.table.SourceCatalog)
+        - background: model of background subtracted from exposure (an lsst.afw.math.BackgroundList)
+        - psfCellSet: spatial cells of PSF candidates (an lsst.afw.math.SpatialCellSet)
+        """
+        self._frame = self._initialFrame  # reset debug display frame
+
+        if not self.config.doMeasurePsf and not exposure.hasPsf():
+            self.log.warn("Source catalog detected and measured with placeholder or default PSF")
+            self.installSimplePsf.run(exposure=exposure)
+
+        if exposureIdInfo is None:
+            exposureIdInfo = ExposureIdInfo()
+
+        # subtract an initial estimate of background level
+        background = self.background.run(exposure).background
+
+        psfIterations = self.config.psfIterations if self.config.doMeasurePsf else 1
+        for i in range(psfIterations):
+            dmeRes = self.detectMeasureAndEstimatePsf(
+                exposure=exposure,
+                exposureIdInfo=exposureIdInfo,
+                background=background,
+            )
+
+            psf = dmeRes.exposure.getPsf()
+            psfSigma = psf.computeShape().getDeterminantRadius()
+            psfDimensions = psf.computeImage().getDimensions()
+            medBackground = np.median(dmeRes.background.getImage().getArray())
+            self.log.info("iter %s; PSF sigma=%0.2f, dimensions=%s; median background=%0.2f" %
+                          (i + 1, psfSigma, psfDimensions, medBackground))
+
+        self.display("psf", exposure=dmeRes.exposure, sourceCat=dmeRes.sourceCat)
+
+        # perform final repair with final PSF
+        self.repair.run(exposure=dmeRes.exposure)
+        self.display("repair", exposure=dmeRes.exposure, sourceCat=dmeRes.sourceCat)
+
+        # perform final measurement with final PSF
+        self.measurement.run(measCat=dmeRes.sourceCat, exposure=dmeRes.exposure,
+                             exposureId=exposureIdInfo.expId)
+        if self.config.doApCorr:
+            apCorrMap = self.measureApCorr.run(exposure=dmeRes.exposure,
+                                               catalog=dmeRes.sourceCat).apCorrMap
+            dmeRes.exposure.getInfo().setApCorrMap(apCorrMap)
+            self.applyApCorr.run(catalog=dmeRes.sourceCat,
+                                 apCorrMap=exposure.getInfo().getApCorrMap())
+        self.catalogCalculation.run(dmeRes.sourceCat)
+
+        self.display("measure", exposure=dmeRes.exposure, sourceCat=dmeRes.sourceCat)
+
+        return pipeBase.Struct(
+            exposure=dmeRes.exposure,
+            sourceCat=dmeRes.sourceCat,
+            background=dmeRes.background,
+            psfCellSet=dmeRes.psfCellSet,
+            characterized=dmeRes.exposure,
+            backgroundModel=dmeRes.background,
+            psfSuccess=dmeRes.psfSuccess
+        )
+
+    @pipeBase.timeMethod
+    def detectMeasureAndEstimatePsf(self, exposure, exposureIdInfo, background):
+        """!Perform one iteration of detect, measure and estimate PSF
+
+        Performs the following operations:
+        - if config.doMeasurePsf or not exposure.hasPsf():
+            - install a simple PSF model (replacing the existing one, if need be)
+        - interpolate over cosmic rays with keepCRs=True
+        - estimate background and subtract it from the exposure
+        - detect, deblend and measure sources, and subtract a refined background model;
+        - if config.doMeasurePsf:
+            - measure PSF
+        @param[in,out] exposure  exposure to characterize (an lsst.afw.image.ExposureF or similar)
+            The following changes are made:
+            - update or set psf
+            - update detection and cosmic ray mask planes
+            - subtract background
+        @param[in] exposureIdInfo  ID info for exposure (an lsst.obs_base.ExposureIdInfo)
+        @param[in,out] background  initial model of background already subtracted from exposure
+            (an lsst.afw.math.BackgroundList).
+        @return pipe_base Struct containing these fields, all from the final iteration
+        of detect sources, measure sources and estimate PSF:
+        - exposure  characterized exposure; image is repaired by interpolating over cosmic rays,
+            mask is updated accordingly, and the PSF model is set
+        - sourceCat  detected sources (an lsst.afw.table.SourceCatalog)
+        - background  model of background subtracted from exposure (an lsst.afw.math.BackgroundList)
+        - psfCellSet  spatial cells of PSF candidates (an lsst.afw.math.SpatialCellSet)
+        """
+        # Install a simple PSF model, if needed or wanted
+        if not exposure.hasPsf() or (self.config.doMeasurePsf and self.config.useSimplePsf):
+            self.log.warn("Source catalog detected and measured with placeholder or default PSF")
+            self.installSimplePsf.run(exposure=exposure)
+
+        # Run repair, but do not interpolate over cosmic rays (do that later)
+        if self.config.requireCrForPsf:
+            self.repair.run(exposure=exposure, keepCRs=True)
+        else:
+            try:
+                self.repair.run(exposure=exposure, keepCRs=True)
+            except LengthError:
+                self.log.warn("Skipping cosmic ray detection: Too many CR pixels (max %0.f)" %
+                              self.config.repair.cosmicray.nCrPixelMax)
+
+        self.display("repair_iter", exposure=exposure)
+
+        if background is None:
+            background = BackgroundList()
+
+        # sourceIdFactory = exposureIdInfo.makeSourceIdFactory()
+        sourceIdFactory = IdFactory.makeSource(exposureIdInfo.expId, exposureIdInfo.unusedBits)
+        table = SourceTable.make(self.schema, sourceIdFactory)
+        table.setMetadata(self.algMetadata)
+
+        # Detect sources
+        detRes = self.detection.run(table=table, exposure=exposure, doSmooth=True)
+        sourceCat = detRes.sources
+        if detRes.fpSets.background:
+            for bg in detRes.fpSets.background:
+                background.append(bg)
+
+        # Deblend sources
+        if self.config.doDeblend:
+            self.deblend.run(exposure=exposure, sources=sourceCat)
+
+        # Measure sources
+        self.measurement.run(measCat=sourceCat, exposure=exposure, exposureId=exposureIdInfo.expId)
+
+        measPsfRes = pipeBase.Struct(cellSet=None)
+        psfSuccess = True
+
+        # Attempt to measure the PSF
+        if self.config.doMeasurePsf:
+            if self.measurePsf.usesMatches:
+                matches = self.ref_match.loadAndMatch(exposure=exposure,
+                                                      sourceCat=sourceCat).matches
+            else:
+                matches = None
+            try:
+                measPsfRes = self.measurePsf.run(exposure=exposure, sources=sourceCat,
+                                                 matches=matches, expId=exposureIdInfo.expId)
+            except Exception:
+                psfSuccess = False
+
+        self.display("measure_iter", exposure=exposure, sourceCat=sourceCat)
+
+        return pipeBase.Struct(
+            exposure=exposure,
+            sourceCat=sourceCat,
+            background=background,
+            psfCellSet=measPsfRes.cellSet,
+            psfSuccess=psfSuccess
+        )
---
+++ src/huntsman/drp/lsst/tasks/processCcd.py
@@ -0,0 +1,75 @@
+""" Class overrides for ProcessCcdTask. """
+import lsst.pipe.base as pipeBase
+from lsst.pipe.tasks.processCcd import ProcessCcdTask
+
+
+class HuntsmanProcessCcdTask(ProcessCcdTask):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @pipeBase.timeMethod
+    def runDataRef(self, sensorRef):
+        """Process one CCD
+        The sequence of operations is:
+        - remove instrument signature
+        - characterize image to estimate PSF and background
+        - calibrate astrometry and photometry
+        @param sensorRef: butler data reference for raw data
+        @return pipe_base Struct containing these fields:
+        - charRes: object returned by image characterization task; an lsst.pipe.base.Struct
+            that will include "background" and "sourceCat" fields
+        - calibRes: object returned by calibration task: an lsst.pipe.base.Struct
+            that will include "background" and "sourceCat" fields
+        - exposure: final exposure (an lsst.afw.image.ExposureF)
+        - background: final background model (an lsst.afw.math.BackgroundList)
+        """
+        self.log.info("Processing %s" % (sensorRef.dataId))
+
+        # Default return values
+        exposure = None
+        background = None
+        charRes = None
+        calibRes = None
+
+        # Instrument signature removal
+        isrSuccess = True
+        try:
+            exposure = self.isr.runDataRef(sensorRef).exposure
+        except Exception:
+            isrSuccess = False
+
+        # Characterise image
+        charSuccess = None
+        if isrSuccess:
+            try:
+                charRes = self.charImage.runDataRef(dataRef=sensorRef, exposure=exposure,
+                                                    doUnpersist=False)
+                exposure = charRes.exposure
+                background = charRes.background
+                charSuccess = True
+            except Exception:
+                charSuccess = False
+
+        # Do image calibration (astrometry + photometry)
+        calibSuccess = None
+        if self.config.doCalibrate and charSuccess:
+            try:
+                calibRes = self.calibrate.runDataRef(
+                    dataRef=sensorRef, exposure=charRes.exposure, background=charRes.background,
+                    doUnpersist=False, icSourceCat=charRes.sourceCat)
+                background = calibRes.background
+                calibSuccess = True
+
+            except Exception:
+                calibSuccess = False
+
+        return pipeBase.Struct(
+            charRes=charRes,
+            calibRes=calibRes,
+            exposure=exposure,
+            background=background,
+            calibSuccess=calibSuccess,
+            charSucess=charSuccess,
+            isrSuccess=isrSuccess,
+        )
