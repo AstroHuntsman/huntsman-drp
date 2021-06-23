0996e1f8c09b98fe343475a0cc4d0c8b3523c9aa
Dan <danjampro@sky.com>
2021-06-23 20:03:07 +1000

    tweak ingestor and update script

--- src/huntsman/drp/collection.py
+++ src/huntsman/drp/collection.py
@@ -96,8 +96,9 @@ class Collection(HuntsmanBase):
             document_filter.update({self._date_key: date_constraint})
 
         # Screen the results if necessary
+        # TODO: Move to raw exposure table
         if screen:
-            document_filter[METRIC_SUCCESS_FLAG] = True  # TODO: Move to raw exposure table
+            document_filter[f"metrics.{METRIC_SUCCESS_FLAG}"] = True
 
         mongo_filter = document_filter.to_mongo()
 
